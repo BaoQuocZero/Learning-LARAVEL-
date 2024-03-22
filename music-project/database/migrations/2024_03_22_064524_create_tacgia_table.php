431049b563ec47985c3d9f0225cc563902ba9d56
BaoQuocZero <130838918+BaoQuocZero@users.noreply.github.com>
2024-03-22 14:25:20 +0700

    tao model

--- music-project/database/migrations/2024_03_22_064524_create_tacgia_table.php
+++ music-project/database/migrations/2024_03_22_064524_create_tacgia_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -14,8 +13,10 @@ return new class extends Migration
     public function up()
     {
         Schema::create('tacgia', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->increments('ma_tgia');
+            $table->string('ten_tgia', 100);
+            $table->string('hinh_tgia');
+
         });
     }
 
